3b324b0968079353e674f4aa77c779d76a9c4ac6
sirhovnanyan <hovnanyansiranush@gmail.com>
2021-11-02 21:05:48 +0400

    Commit number seven

--- src/tree/Main.java
+++ src/tree/Main.java
@@ -2,7 +2,7 @@ package tree;
 
 public class Main {
     public static void main(String[] args) {
-        Tree<Integer> tree = new DefaultTree<>();
+        Tree<User> tree = new DefaultTree<>();
 
 /*        tree.addNode(10);
         tree.addNode(20);
@@ -20,6 +20,16 @@ public class Main {
 
         System.out.println(tree.containsNode(18));*/
 
+        tree.addNode(new User(10, "Me"));
+        tree.addNode(new User(15, "You"));
+        tree.addNode(new User(20, "We"));
+        tree.addNode(new User(30, "He"));
+        tree.addNode(new User(26, "aa"));
+        tree.addNode(new User(16, "ab"));
+        tree.addNode(new User(26, "a"));
+        tree.addNode(new User(9, "ac"));
+        tree.addNode(new User(5, "ba"));
+        tree.addNode(new User(6, "ra"));
 
 
         System.out.println(tree);
